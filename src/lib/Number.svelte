a49d016ac156500e743cbe33a541b3f50a674873
Igor Štumberger <hi@igorstumberger.com>
2024-01-09 19:02:59 +0100

    MVP

---
+++ src/lib/Number.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let number;
+
+	$: formattedNumber = number.toLocaleString('de-DE', {
+		notation: 'standard',
+		compactDisplay: 'short'
+	});
+</script>
+
+<span>{formattedNumber}</span>
+
+<style>
+	span {
+		font-variant-numeric: tabular-nums;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,324 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Number from '../lib/Number.svelte';
+
+	let price_blok_1 = 4.41;
+	let price_blok_2 = 1.08;
+	let price_blok_3 = 0.23;
+	let price_blok_4 = 0.02;
+	let price_blok_5 = 0.0;
+</script>
+
+<div class="p-2">
+	<h1 class=" text-xl font-medium">Cena elektrike po urah v dnevu</h1>
+
+	<h2 class="font-medium pt-4">November, December, Januar, Februar</h2>
+	<h3 class="pt-2">Delovni dan</h3>
+	<div class="grid grid-cols-24">
+		{#each { length: 24 } as _, i}
+			<div
+				class="grid col-start-[{i + 1}] col-end-[{i +
+					2}] text-xs py-1 border-l border-slate-500 pl-1 text-slate-700"
+			>
+				{i}:00
+			</div>
+		{/each}
+		<div class="grid col-span-6 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-1 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-7 border-l border-slate-500">
+			<div class="blok blok-1"><Number number={price_blok_1} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-4 border-l border-slate-500">
+			<div class="blok blok-1"><Number number={price_blok_1} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+	</div>
+	<h3 class="pt-2">Dela prost dan</h3>
+	<div class="grid grid-cols-24">
+		{#each { length: 24 } as _, i}
+			<div
+				class="grid col-start-[{i + 1}] col-end-[{i +
+					2}] text-xs py-1 border-l border-slate-500 pl-1 text-slate-700"
+			>
+				{i}:00
+			</div>
+		{/each}
+		<div class="grid col-span-6 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-1 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-7 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-4 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+	</div>
+
+	<h2 class="font-medium pt-8">Marec, April, Maj, Junij, Julij, Avgust, September, Oktober</h2>
+	<h3 class="pt-2">Delovni dan</h3>
+	<div class="grid grid-cols-24">
+		{#each { length: 24 } as _, i}
+			<div
+				class="grid col-start-[{i + 1}] col-end-[{i +
+					2}] text-xs py-1 border-l border-slate-500 pl-1 text-slate-700"
+			>
+				{i}:00
+			</div>
+		{/each}
+		<div class="grid col-span-6 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-1 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-7 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-4 border-l border-slate-500">
+			<div class="blok blok-2"><Number number={price_blok_2} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+	</div>
+	<h3 class="pt-2">Dela prost dan</h3>
+	<div class="grid grid-cols-24">
+		{#each { length: 24 } as _, i}
+			<div
+				class="grid col-start-[{i + 1}] col-end-[{i +
+					2}] text-xs py-1 border-l border-slate-500 pl-1 text-slate-700"
+			>
+				{i}:00
+			</div>
+		{/each}
+		<div class="grid col-span-6 border-l border-slate-500">
+			<div class="blok blok-5"><Number number={price_blok_5} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-1 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-7 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-4 border-l border-slate-500">
+			<div class="blok blok-3"><Number number={price_blok_3} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-4"><Number number={price_blok_4} /><span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-5"><Number number={price_blok_5} /><span class="value">€/kW</span></div>
+		</div>
+	</div>
+
+	<div class="flex gap-4 pt-10 col-span-full">
+		<div class="flex items-center gap-1.5 text-xs">
+			<div class="blok-1 h-full w-3"></div>
+			<div class="flex-col items-center p-0 m-0">
+				Blok 1
+				<span class="value">
+					<input type="number" step=".01" bind:value={price_blok_1} class="w-10" />€/kW
+				</span>
+			</div>
+		</div>
+		<div class="flex items-center gap-1.5 text-xs">
+			<div class="blok-2 h-full w-3"></div>
+			<div class="flex-col items-center p-0 m-0">
+				Blok 2
+				<span class="value">
+					<input type="number" step=".01" bind:value={price_blok_2} class="w-10" />€/kW
+				</span>
+			</div>
+		</div>
+		<div class="flex items-center gap-1.5 text-xs">
+			<div class="blok-3 h-full w-3"></div>
+			<div class="flex-col items-center p-0 m-0">
+				Blok 3
+				<span class="value">
+					<input type="number" step=".01" bind:value={price_blok_3} class="w-10" />€/kW
+				</span>
+			</div>
+		</div>
+		<div class="flex items-center gap-1.5 text-xs">
+			<div class="blok-4 h-full w-3"></div>
+			<div class="flex-col items-center p-0 m-0">
+				Blok 4
+				<span class="value">
+					<input type="number" step=".01" bind:value={price_blok_4} class="w-10" />€/kW
+				</span>
+			</div>
+		</div>
+		<div class="flex items-center gap-1.5 text-xs">
+			<div class="blok-5 h-full w-3"></div>
+			<div class="flex-col items-center p-0 m-0">
+				Blok 5
+				<span class="value">
+					<input type="number" step=".01" bind:value={price_blok_5} class="w-10" />€/kW
+				</span>
+			</div>
+		</div>
+	</div>
+
+	<!-- <div class="grid grid-cols-24">
+		{#each { length: 24 } as _, i}
+			<div
+				class="grid col-start-[{i + 1}] col-end-[{i +
+					2}] text-xs py-1 border-l border-slate-500 pl-1 text-slate-700"
+			>
+				{i}:00
+			</div>
+		{/each}
+		<div class="grid col-span-6 border-l border-slate-500">
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+			<div class=" spaces"></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+			<div class="blok blok-5">{price_blok_5}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-1 border-l border-slate-500">
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-7 border-l border-slate-500">
+			<div class="blok blok-1">{price_blok_1}<span class="value">€/kW</span></div>
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-2">{price_blok_1}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-4 border-l border-slate-500">
+			<div class="blok blok-1">{price_blok_1}<span class="value">€/kW</span></div>
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-2">{price_blok_1}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-2">{price_blok_2}<span class="value">€/kW</span></div>
+			<div class="blok blok-3">{price_blok_3}<span class="value">€/kW</span></div>
+		</div>
+		<div class="grid col-span-2 border-l border-slate-500">
+			<div class="blok blok-3">{price_blok_1}<span class="value">€/kW</span></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+			<div class="spaces"></div>
+			<div class="blok blok-4">{price_blok_4}<span class="value">€/kW</span></div>
+			<div class="blok blok-5">{price_blok_5}<span class="value">€/kW</span></div>
+		</div>
+		<div class="flex gap-4 py-4 col-span-full">
+			<div class="flex items-center gap-1.5 text-xs">
+				<div class="bg-red-400 h-full w-3"></div>
+				<div class="flex-col items-center p-0 m-0">
+					Blok 1
+					<span class="value">{price_blok_1}€/kW</span>
+				</div>
+			</div>
+			<div class="flex items-center gap-1.5 text-xs">
+				<div class="bg-red-400 h-full w-3"></div>
+				<div class="flex-col items-center p-0 m-0">
+					Blok 2
+					<span class="value">{price_blok_2}€/kW</span>
+				</div>
+			</div>
+			<div class="flex items-center gap-1.5 text-xs">
+				<div class="bg-red-400 h-full w-3"></div>
+				<div class="flex-col items-center p-0 m-0">
+					Blok 3
+					<span class="value">{price_blok_3}€/kW</span>
+				</div>
+			</div>
+			<div class="flex items-center gap-1.5 text-xs">
+				<div class="bg-red-400 h-full w-3"></div>
+				<div class="flex-col items-center p-0 m-0">
+					Blok 4
+					<span class="value">{price_blok_4}€/kW</span>
+				</div>
+			</div>
+			<div class="flex items-center gap-1.5 text-xs">
+				<div class="bg-red-400 h-full w-3"></div>
+				<div class="flex-col items-center p-0 m-0">
+					Blok 5
+					<span class="value">{price_blok_5}€/kW</span>
+				</div>
+			</div>
+		</div>
+	</div> -->
+</div>
+
+<style>
+	.spaces {
+		@apply min-h-3;
+	}
+
+	.value {
+		@apply block opacity-50 font-normal text-xs;
+	}
+
+	.blok {
+		@apply text-center text-sm font-semibold py-1.5;
+	}
+
+	.blok-1 {
+		@apply bg-red-500;
+	}
+
+	.blok-2 {
+		@apply bg-orange-400;
+	}
+
+	.blok-3 {
+		@apply bg-amber-300;
+	}
+
+	.blok-4 {
+		@apply bg-green-300;
+	}
+
+	.blok-5 {
+		@apply bg-emerald-100;
+	}
+</style>
